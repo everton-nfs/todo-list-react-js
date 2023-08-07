b9da84dad521f4dc9d67c328f883dc385269efb5
Everton Novaes <evertonnfs2@gmail.com>
2023-08-07 10:16:14 -0300

    colors in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
-export function Home(){
+export function Home() {
   return (
-    <h1>Home</h1>
+    <div className="bg-[#1e1530ff] h-screen">
+      <div className="mx-auto max-w-screen-md xl:max-w-screen-xs">
+        <h1 className="uppercase text-white py-5">Todo List</h1>
+        <div className="grid grid-cols-1 pb-8 bg-[#382952ff] backdrop-blur-sm md:grid-cols-12">
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
