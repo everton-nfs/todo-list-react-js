2dd0486feec76394e1100b512e19f29b9992e117
Everton Novaes <evertonnfs2@gmail.com>
2023-08-16 13:08:22 -0300

    Delete Functionality

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,33 @@
-export function Item() {
-  return (
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 
-    <div className="bg-[#382952ff] p-4 mb-2">
-      <h1 className="text-white">Sou uma lista!</h1>
+import { MdModeEdit, MdDelete} from "react-icons/md";
+
+export function Item(props) {
+
+
+  const handleDelete = () => {
+    const updatedItems = props.items.filter((item) => item.id !== props.id);
+    props.setItems(updatedItems);
+    localStorage.setItem('items', JSON.stringify(updatedItems));
+  };
+
+  return (
+    <div className="bg-[#382952ff] mb-2 flex flex-row h-14">
+      <div className="flex justify-start items-center pl-4 basis-[93%] overflow-hidden">
+        <h1 className="overflow-hidden whitespace-nowrap text-ellipsis text-white">{props.title}</h1>
+      </div>
+
+     
+   
+      <div className="flex items-center gap-2 text-white cursor-pointer text-lg basis-[7%] p-3">
+        <button onClick={() => alert("Vai editar!")}>
+          <MdModeEdit/>
+        </button>
+        <button onClick={handleDelete}>
+          <MdDelete/>
+        </button>
+      </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,68 @@
+import { useEffect, useState } from "react";
 import { Item } from "../components/Item";
 
 export function Home() {
+
+  const [items, setItems] = useState([]);
+  const [inputTitle, setInputTitle] = useState('');
+
+  useEffect(() => {
+    const storedItems = localStorage.getItem('items');
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+
+  const handleAdd = () => {
+    const newItem = {
+      id: Date.now(),
+      title: inputTitle,
+    };
+
+    const updatedItems = [...items, newItem];
+    setItems(updatedItems);
+    localStorage.setItem('items', JSON.stringify(updatedItems));
+    setInputTitle('');
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleAdd();
+    }
+  };
+
+  //console.log(JSON.stringify(items, null, '\t'));
+
   return (
     <div className="bg-[#1e1530ff] h-screen">
       <div className="mx-auto max-w-screen-md xl:max-w-screen-xs">
         <h1 className="uppercase text-white py-5">Todo List</h1>
 
-        <input type="text" className={`w-full px-4 py-[3px] text-black bg-gray-25 border border-gray-25 focus:border-white focus:outline-none placeholder:text-gray-30 text-base mb-2`}/>
+        <input 
+          type="text"
+          onChange={(e) => setInputTitle(e.target.value)}
+          placeholder="Digite sua tarefa!"
+          onKeyDown={handleKeyPress}
+          className={`w-full px-4 py-[3px] text-black bg-gray-25 border border-gray-25 focus:border-white focus:outline-none placeholder:text-gray-30 text-base mb-2`}
+        />
+
+        <button 
+          onClick={handleAdd}
+        >
+          Add String
+        </button>
       
-        <Item/>
-        <Item/>
+        {items.map((item) => (
+          <Item 
+            key={item.id} 
+            id={item.id} 
+            title={item.title}
+            items={items}
+            setItems={setItems}
+          />
+        ))}
+
       </div>
-
-
 
     </div>
   )
