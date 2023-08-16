56252a8d79ae24a5c2fecbd25d204e3191a2652f
Everton Novaes <evertonnfs2@gmail.com>
2023-08-16 09:11:00 -0300

    Adding list item without functionality

---
+++ src/components/Item.jsx
@@ -0,0 +1,8 @@
+export function Item() {
+  return (
+
+    <div className="bg-[#382952ff] p-4 mb-2">
+      <h1 className="text-white">Sou uma lista!</h1>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
+import { Item } from "../components/Item";
+
 export function Home() {
   return (
     <div className="bg-[#1e1530ff] h-screen">
       <div className="mx-auto max-w-screen-md xl:max-w-screen-xs">
         <h1 className="uppercase text-white py-5">Todo List</h1>
-        <div className="grid grid-cols-1 pb-8 bg-[#382952ff] backdrop-blur-sm md:grid-cols-12">
 
-        </div>
-
+        <input type="text" className={`w-full px-4 py-[3px] text-black bg-gray-25 border border-gray-25 focus:border-white focus:outline-none placeholder:text-gray-30 text-base mb-2`}/>
+      
+        <Item/>
+        <Item/>
       </div>
+
+
 
     </div>
   )
