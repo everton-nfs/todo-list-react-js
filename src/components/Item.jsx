0df516f72b71cc73f64a2c2ef4d9a48783052f88
Everton Novaes <evertonnfs2@gmail.com>
2023-08-16 17:39:40 -0300

    Adding task checks

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,43 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 
-import { MdModeEdit, MdDelete} from "react-icons/md";
+import { useState } from "react";
+import { MdModeEdit, MdDelete, MdSave } from "react-icons/md";
+import { BsCheckLg } from "react-icons/bs";
 
 export function Item(props) {
 
+  const [edit, setEdit] = useState(false);
+  const [title, setTitle] = useState(props.title);
+  const [checked, setChecked] = useState(props.checked)
+
+  const handleCheck = () => {
+    const updatedItems = props.items.map((item) => {
+      if (item.id === props.id) {
+        return { ...item, checked: !checked };
+      }
+      return item;
+    });
+
+    props.setItems(updatedItems);
+    localStorage.setItem('items', JSON.stringify(updatedItems));
+
+    setChecked(!checked)
+  };
+
+  const handleSave = () => {
+    const updatedItems = props.items.map((item) => {
+      if (item.id === props.id) {
+        return { ...item, title: title };
+      }
+      return item;
+    });
+
+    props.setItems(updatedItems);
+    localStorage.setItem('items', JSON.stringify(updatedItems));
+
+    setEdit(false);
+  };
 
   const handleDelete = () => {
     const updatedItems = props.items.filter((item) => item.id !== props.id);
@@ -12,20 +45,59 @@ export function Item(props) {
     localStorage.setItem('items', JSON.stringify(updatedItems));
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSave();
+    }
+  };
+
   return (
-    <div className="bg-[#382952ff] mb-2 flex flex-row h-14">
-      <div className="flex justify-start items-center pl-4 basis-[93%] overflow-hidden">
-        <h1 className="overflow-hidden whitespace-nowrap text-ellipsis text-white">{props.title}</h1>
+    <div className={`${checked ? "bg-[#30d89b]" : "bg-[#8847a3]"} mb-2 flex flex-row h-14`}>
+
+      <div className="flex justify-center items-center basis-[8%] px-3">
+        <button 
+          onClick={handleCheck} 
+          className={`${checked ? "text-[#30d89b] bg-white" : "bg-transparent"} rounded-full w-6 h-6 border-2 border-white flex justify-center items-center text-lg pr-[2px]`}>
+          {checked ? <BsCheckLg/> : <></>}
+        </button>
       </div>
 
-     
-   
-      <div className="flex items-center gap-2 text-white cursor-pointer text-lg basis-[7%] p-3">
-        <button onClick={() => alert("Vai editar!")}>
-          <MdModeEdit/>
-        </button>
-        <button onClick={handleDelete}>
-          <MdDelete/>
+
+      <div className="flex justify-start items-center basis-[82%] overflow-hidden">
+        {
+          edit ? (
+            <input
+              type="text"
+              autoFocus
+              className='w-full bg-transparent text-white focus:outline-none placeholder:text-gray-30'
+              value={title}
+              onKeyDown={handleKeyPress}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          ) : (
+            <h1
+              onDoubleClick={() => setEdit(true)}
+              className="overflow-hidden whitespace-nowrap text-ellipsis text-white cursor-text">
+              {title}
+            </h1>
+          )
+        }
+      </div>
+
+      <div className="flex items-center gap-4 text-white cursor-pointer text-lg basis-[10%] p-3">
+        {
+          edit ? (
+            <button onClick={handleSave} className="h-full">
+              <MdSave />
+            </button>
+          ) : (
+            <button onClick={() => setEdit(true)} className="h-full">
+              <MdModeEdit />
+            </button>
+          )
+        }
+        <button onClick={handleDelete} className="h-full">
+          <MdDelete />
         </button>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Item } from "../components/Item";
+import { TbPlus} from "react-icons/tb";
 
 export function Home() {
 
   const [items, setItems] = useState([]);
   const [inputTitle, setInputTitle] = useState('');
+  const inputRef = useRef(null);
 
   useEffect(() => {
     const storedItems = localStorage.getItem('items');
@@ -17,12 +19,14 @@ export function Home() {
     const newItem = {
       id: Date.now(),
       title: inputTitle,
+      checked: false
     };
 
     const updatedItems = [...items, newItem];
     setItems(updatedItems);
     localStorage.setItem('items', JSON.stringify(updatedItems));
-    setInputTitle('');
+    setInputTitle("");
+    inputRef.current.focus();
   };
 
   const handleKeyPress = (event) => {
@@ -34,29 +38,37 @@ export function Home() {
   //console.log(JSON.stringify(items, null, '\t'));
 
   return (
-    <div className="bg-[#1e1530ff] h-screen">
-      <div className="mx-auto max-w-screen-md xl:max-w-screen-xs">
-        <h1 className="uppercase text-white py-5">Todo List</h1>
+    <div className="bg-[#762b94] h-screen">
+      <div className="mx-auto max-w-screen-md xl:max-w-screen-xs p-5">
+        <h1 className="uppercase text-white py-5 text-2xl">Todo List</h1>
+
+        <div className="flex justify-center items-center mb-7">
 
         <input 
+          autoFocus
           type="text"
           onChange={(e) => setInputTitle(e.target.value)}
+          value={inputTitle}
           placeholder="Digite sua tarefa!"
-          onKeyDown={handleKeyPress}
-          className={`w-full px-4 py-[3px] text-black bg-gray-25 border border-gray-25 focus:border-white focus:outline-none placeholder:text-gray-30 text-base mb-2`}
+          onKeyDown={inputTitle.length > 0 && handleKeyPress}
+          ref={inputRef}
+          className={`w-full px-4 py-2 text-black bg-gray-25 border border-gray-25 focus:border-white focus:outline-none placeholder:text-gray-30 text-base`}
         />
 
         <button 
-          onClick={handleAdd}
+          onClick={inputTitle.length > 0 && handleAdd}
+          className="text-white bg-[#8847a3] h-[2.625rem] flex justify-center items-center w-11 text-lg"
         >
-          Add String
+          <TbPlus/>
         </button>
       
+      </div>
         {items.map((item) => (
           <Item 
             key={item.id} 
             id={item.id} 
             title={item.title}
+            checked={item.checked}
             items={items}
             setItems={setItems}
           />
